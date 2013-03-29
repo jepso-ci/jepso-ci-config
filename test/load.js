6ce2803a553afd7c0eb09f4ba8dbbca9766b10e5
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-29 16:47:40 +0000

    Add tests for error cases

--- test/load.js
+++ test/load.js
@@ -13,6 +13,16 @@ describe('load remote', function () {
         assert(res.url === '/test.html');
       });
   });
+  it('errors if the config can\'t be found', function () {
+    this.timeout(10000);
+    this.slow(2000);
+    return conf.loadRemote('foo', 'bar', 'master')
+      .then(function (res) {
+        throw new Error('Should have failed');
+      }, function (err) {
+        return;
+      });
+  });
 });
 
 describe('load local', function () {
@@ -25,11 +35,32 @@ describe('load local', function () {
         assert(res.url === '/test.html');
       });
   });
-  it('loads the jepso-ci config of a directory', function () {
+  it('loads the jepso-ci config of a directory syncronously', function () {
     this.timeout(2000);
     this.slow(200);
     var res = conf.loadLocal.sync(join(__dirname, 'fixture'))
     assert(conf.validate(res));
     assert(res.url === '/test.html');
+  });
+
+  it('errors if the config can\'t be found', function () {
+    this.timeout(10000);
+    this.slow(2000);
+    return conf.loadLocal(join(__dirname, 'non-existant'))
+      .then(function (res) {
+        throw new Error('Should have failed');
+      }, function (err) {
+        return;
+      });
+  });
+  it('errors if the config can\'t be found syncronously', function () {
+    this.timeout(10000);
+    this.slow(2000);
+    try {
+      conf.loadLocal.sync(join(__dirname, 'non-existant'))
+    } catch (ex) {
+      return;
+    }
+    throw new Error('Should have failed');
   });
 });
